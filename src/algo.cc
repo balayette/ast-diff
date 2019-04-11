4a0f00ba8729e89436641611fbefcf17727d12ca
Nicolas Manichon <nicolas.manichon@lse.epita.fr>
2019-04-11 20:32:22 +0200

    algo: Implement the bottom up phase

--- src/algo.cc
+++ src/algo.cc
@@ -62,15 +62,79 @@ Mappings topDown(Tree *T1, Tree *T2) {
   return M;
 }
 
+double dice(Tree *t1, Tree *t2, Mappings &M) {
+  auto nbr = 0;
+
+  for (auto *desc : GetDescendants(t1)) {
+    auto *dest = M.GetDestination(desc);
+    if (!dest)
+      continue;
+    if (!dest->IsDescendantOf(t2))
+      continue;
+
+    nbr++;
+  }
+
+  double d = ((double)nbr * 2.0) /
+             (GetDescendants(t1).size() + GetDescendants(t2).size());
+  std::cout << "D: " << d << '\n';
+  return d;
+}
+
+Tree *candidate(Tree *t1, Tree *T2, Mappings &M) {
+  Tree *best = nullptr;
+  double bestSim = 0.0;
+
+  T2->PreorderTraversal([&](Tree *c) {
+    if (c->GetValue() != t1->GetValue())
+      return;
+    if (M.ContainsDestinationMapping(c))
+      return;
+
+    double s = dice(t1, c, M);
+    if (s > bestSim) {
+      bestSim = s;
+      best = c;
+    }
+  });
+
+  return best;
+}
+
 void bottomUp(Tree *T1, Tree *T2, Mappings &M) {
-  (void)T1;
-  (void)T2;
-  (void)M;
+  Tree::vecptr descendantsPost;
+  T1->PostorderTraversal([&](Tree *curr) {
+    if (M.ContainsSourceMapping(curr))
+      return;
+
+    for (auto &child : curr->GetChildren()) {
+      if (!M.ContainsSourceMapping(child.get()))
+        continue;
+
+      descendantsPost.push_back(curr);
+      return;
+    }
+  });
+
+  for (auto *t1 : descendantsPost) {
+    auto *t2 = candidate(t1, T2, M);
+    if (!t2) {
+      std::cout << "Didn't find a candidate\n";
+      continue;
+    }
+    M.AddMapping(t1, t2);
+    // FIXME: No optimal mappings.
+  }
 }
 
 Mappings Gumtree(Tree *T1, Tree *T2) {
   auto mappings = topDown(T1, T2);
   bottomUp(T1, T2, mappings);
+  std::cout << "Similarity: "
+            << (2 * mappings.size() /
+                ((double)GetDescendants(T1).size() + 1 +
+                 GetDescendants(T2).size() + 1))
+            << '\n';
   return mappings;
 }
 
